87a7f1f9a20f87a0672b65b4a0c94be30d2b8ca4
BugDroid-Duarte <duarte.mrandrade@gmail.com>
2018-10-10 02:08:36 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+function isEven(n) {
+    return n % 2 == 0;
+ }
+
+ function isOdd(n) {
+    return Math.abs(n % 2) == 1;
+ }
+
+var array = [];
+
+for (i = 0; i <= 9; i++) {
+    var random = Math.floor((Math.random() * 10) + 1);
+    array[i] = random;
+}
+
+function generateNum (array, randNumber, randNumber2) {
+   while (array.length < 10) {
+     if(array.length == 8 || 9) {
+       array.push(randNumber2);
+       generateNum(array, randNumber, randNumber);
+     } else {
+       array.push(randNumber);
+       generateNum(array, randNumber, randNumber2);
+     }
+   }
+   return array;
+}
+
+var array = [];
+
+for (i = 0; i <= 9; i++) {
+    var random = Math.floor((Math.random() * 10) + 1);
+    array[i] = random;
+}
+
+array.pop();
+array.pop();
+array.pop();
+
+var randNumber = array[Math.floor(Math.random()*array.length) + 1];
+var randNumber2 = array[Math.floor(Math.random()*array.length) + 1];
+
+generateNum(array, randNumber, randNumber2);
+
+array.forEach(element => {
+    console.log(element);
+});
+
+var odd = [];
+var even = [];
+
+array.forEach(element => {
+    if (isEven(element)) {
+        even.push(element);
+    } else {
+        odd.push(element);
+    }
+});
+
+odd.forEach(element => {
+    var li = "<li>" + element + "</li>"
+    $('.left ul').append(li);
+});
+
+even.forEach(element => {
+    var li = "<li>" + element + "</li>"
+    $('.right ul').append(li);
+});
